480e352a564854d076adb2f9a17ff597a8bc3cb7
Sosa, Jose <Jose.Sosa@bendix.com>
2025-03-26 12:41:12 -0600

    Add filter for company name and symbol in stock

--- api/Controllers/StockController.cs
+++ api/Controllers/StockController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using api.Data;
 using api.Dtos.Stock;
+using api.Helpers;
 using api.Interfaces;
 using api.Mapper;
 using api.Models;
@@ -25,12 +26,12 @@ namespace api.Controllers
         }
         
         [HttpGet]
-        public async Task<IActionResult> GetAll()
+        public async Task<IActionResult> GetAll([FromQuery] QueryObject query)
         {
             if (!ModelState.IsValid)
                 return BadRequest(ModelState);
-                
-            var stocks = await _stockRepo.GetAllAsync();
+
+            var stocks = await _stockRepo.GetAllAsync(query);
             var stockDto = stocks.Select(s => s.ToStockDto());
 
             return Ok(stocks);
