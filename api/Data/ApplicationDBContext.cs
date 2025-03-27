88388a1829ce435307f8064abd21c86f21e74606
Sosa, Jose <Jose.Sosa@bendix.com>
2025-03-26 23:16:57 -0600

    add register

--- api/Data/ApplicationDBContext.cs
+++ api/Data/ApplicationDBContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -16,5 +17,26 @@ namespace api.Data
         }
         public DbSet<Stock> Stocks { get; set; }
         public DbSet<Comment> Comments { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            List<IdentityRole> roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Id = new Guid("d2d2d2d2-d2d2-d2d2-d2d2-d2d2d2d2d2d2").ToString(),  // Set a static value, to avoid generating a new value each time that the code is executed
+                    Name = "Admin",
+                    NormalizedName = "ADMIN",
+                },
+                new IdentityRole
+                {
+                    Id = new Guid("e3e3e3e3-e3e3-e3e3-e3e3-e3e3e3e3e3e3").ToString(), // Set a static value, to avoid generating a new value each time that the code is executed
+                    Name = "User",
+                    NormalizedName = "USER",
+                },
+            };
+            builder.Entity<IdentityRole>().HasData(roles);
+        }
     }
 }
---
+++ api/Migrations/20250327054827_SeedRole.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "d2d2d2d2-d2d2-d2d2-d2d2-d2d2d2d2d2d2", null, "Admin", "ADMIN" },
+                    { "e3e3e3e3-e3e3-e3e3-e3e3-e3e3e3e3e3e3", null, "User", "USER" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "d2d2d2d2-d2d2-d2d2-d2d2-d2d2d2d2d2d2");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "e3e3e3e3-e3e3-e3e3-e3e3-e3e3e3e3e3e3");
+        }
+    }
+}
